07d86e6ddde4e35aac29aab3f2d1e40e6e7b8ea4
Caleb Muturi <calebmuturi2024@gmail.com>
2025-11-01 15:44:33 +0300

    created navbar component and mounted  the Navbar on App.jsx on top of Routes to appear on all pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,12 @@ import Product from './pages/Product';
 import Cart from './pages/Cart';
 import Login from './pages/Login';
 import Orders from './pages/Orders';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] lg:px-[9vw]">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/collection" element={<Collection />} />
