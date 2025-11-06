fca6b793673b37226c96c2a450d938d77ee34936
Caleb Muturi <calebmuturi2024@gmail.com>
2025-11-06 16:54:01 +0300

    Display product list , map it with setLatestProducts array , with new component product item

--- src/components/LatestCollection.jsx
+++ src/components/LatestCollection.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import Title from './Title';
+import ProductItem from './ProductItem';
 
 const LatestCollection = () => {
   const { products } = useContext(ShopContext);
@@ -8,7 +9,7 @@ const LatestCollection = () => {
 
   useEffect(() => {
     setLatestProducts(products.slice(0, 10));
-  }, [])
+  }, []);
 
   return (
     <div className="my-10">
@@ -19,7 +20,18 @@ const LatestCollection = () => {
         </p>
       </div>
 
-
+      {/* Rendering Products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProducts.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            image={item.image}
+            name={item.name}
+            price={item.price}
+          />
+        ))}
+      </div>
     </div>
   );
 };
